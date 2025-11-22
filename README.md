29e6107b4f6899ebff3fda6cf74b82e7e693b7df
henryishuman <zuparnowa@gmail.com>
2025-11-22 23:23:22 +0000

    Completion of readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# pokemon-generators
+# Pokémon Sprite Generators
 
+Various pipelines and models that are used to attempt to generate an image of Pokémon.
+
+## Installation
+
+Before attempting to run anything, make sure that all required python packages have been installed by using the following command:
+
+`pip install -r requirements.txt`
+
+## Structure
+
+This project is organised into `models` and `pipelines`. 
+
+`Models` are trained on a `pandas` `DataFrame` containing a list of images represented by four pixel values, each being the colour of said pixel, i.e. `[0, 85, 170, 255]` encoding the colours black, dark grey, light grey, and white.
+
+`Pipelines` are used to build up a series of reads, data transformations, model training, and generation of sprite images.
+
+## Running
+
+So far, the only pipeline which exists is that of the `kernal_model_pipeline`. I am planning on adding further pipelines in the future. To run this, execute the `generate_pokemon_images.py` script, i.e.:
+
+`python generate_pokemon_images.py`
--- pipelines/kernal_model_pipeline.py
+++ pipelines/kernal_model_pipeline.py
@@ -4,18 +4,18 @@ from models.kernel_model import KernelModel
 from utils.image_utils import create_duplicates_of_images, delete_previous_duplications, read_images_as_dataframe, save_image
 
 
-def run_pipeline(image_root_path: str) -> None:
+def run_pipeline(image_root_path: str, output_directory: str = "generated") -> None:
     print("Duplicating images for training...")
     delete_previous_duplications(image_root_path)
     create_duplicates_of_images(image_root_path)
 
     print("Reading images as integer data...")
     df_images = read_images_as_dataframe(image_root_path)
-    if not os.path.exists("generated"):
-        os.mkdir("generated")
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
 
     print("Training kernel models...")
-    kernel_sizes = [9]#[1, 5, 9, 13, 17, 21]
+    kernel_sizes = [1, 2, 3, 4, 5, 7, 11, 13, 11, 7, 5, 4, 3, 2, 1]
     kernel_models = []
     for kernel_size in kernel_sizes:
         kernel_model = KernelModel(kernel_size=kernel_size, is_kernal_centred=kernel_size%2==1)
@@ -24,9 +24,9 @@ def run_pipeline(image_root_path: str) -> None:
 
     print("Generating images from kernel models...")
     generated_image = None
-    for kernel_model in kernel_models:
-        generated_image = kernel_model.generate(epochs = 25, seed=generated_image)
-        save_image(f"./generated/test_kernel_{kernel_model.kernel_size}.png", generated_image)
+    for index, kernel_model in enumerate(kernel_models):
+        generated_image = kernel_model.generate(epochs = 100, seed=generated_image)
+        save_image(f"./{output_directory}/test_kernel_{index}_size_{kernel_model.kernel_size}.png", generated_image)
 
     print("Cleaning up temporary data...")
     delete_previous_duplications(image_root_path)
